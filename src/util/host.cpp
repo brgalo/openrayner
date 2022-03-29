32888d1e07c314951ffd2f2d6a95e79acebc6586
brgalo <martinhwehr@gmail.com>
2022-03-29 19:11:36 +0200

    contttt

--- src/util/host.cpp
+++ src/util/host.cpp
@@ -1,4 +1,5 @@
 
+#include "vulkan/vulkan.hpp"
 #include "host.hpp"
 #include <cstdint>
 #include <cstdlib>
@@ -14,7 +15,7 @@ void Application::initInstance() {
   applicationInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
   applicationInfo.pEngineName = "G&G";
   applicationInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
-  applicationInfo.apiVersion = VK_API_VERSION_1_2; // TODO: see 1.3
+  applicationInfo.apiVersion = VK_API_VERSION_1_3;
 
   // retrieve available layers -> TODO: toggle validation
   vkEnumerateInstanceLayerProperties(&instanceInfo.layerCount, nullptr);
@@ -46,8 +47,8 @@ void Application::initInstance() {
   instanceCreateInfo.pNext = nullptr; // TODO: migght need an info struct for validation layers!
   instanceCreateInfo.flags = 0;
   instanceCreateInfo.pApplicationInfo = &applicationInfo;
-  instanceCreateInfo.enabledLayerCount = instanceInfo.layerCount; // TODO: change if want windows
-  instanceCreateInfo.ppEnabledLayerNames = instanceInfo.requestedLayers.data();
+  instanceCreateInfo.enabledLayerCount = 0;//instanceInfo.layerCount; // TODO: change if want windows
+  instanceCreateInfo.ppEnabledLayerNames = NULL;//instanceInfo.requestedLayers.data();
   instanceCreateInfo.enabledExtensionCount = 0;
   instanceCreateInfo.ppEnabledExtensionNames = NULL;
 
@@ -57,7 +58,105 @@ void Application::initInstance() {
 }
 
 void Application::setupDevices() {
+    // get list of phyical devices
     vkEnumeratePhysicalDevices(instance, &phyDevices.deviceCount, NULL);
     phyDevices.listOfDevices.resize(phyDevices.deviceCount);
+    phyDevices.deviceProperties.resize(phyDevices.deviceCount);
     vkEnumeratePhysicalDevices(instance, &phyDevices.deviceCount, phyDevices.listOfDevices.data());
+    std::cout << "Found " << phyDevices.deviceCount << " devices.\n";
+    for(size_t i = 0; i < phyDevices.deviceCount; ++i) {
+        vkGetPhysicalDeviceProperties(phyDevices.listOfDevices[i], &phyDevices.deviceProperties[i]);
+        std::cout << "Dev " << i << ":\n";
+        std::cout << "Name: " << phyDevices.deviceProperties[i].deviceName << "\n";
+        std::cout << "Type: " << phyDevices.deviceProperties[i].deviceType << "\n";
+        if(phyDevices.deviceProperties[i].deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+            // set device to type 2 -> dicrete GPU
+            device.physDeviceHandle = phyDevices.listOfDevices[i];
+            std::cout << "Set to device: " << i << ".\n";
+        }
+    }
+    // check for RT extenstions
+    std::cout << "\n check device features:\n";
+    vkGetPhysicalDeviceFeatures(device.physDeviceHandle, &device.physDeviceFeatures);
+    // TODO: Check if relevant?
+    std::cout << "missing functionality, also not sure if relevant for Raytracing extensions\n";
+
+    // check memory properties
+    vkGetPhysicalDeviceMemoryProperties(device.physDeviceHandle, &phyDevices.memoryProperties);
+
+    // check queue settings
+    vkGetPhysicalDeviceQueueFamilyProperties(device.physDeviceHandle, &device.queues.queueFamilyCount, NULL);
+    device.queues.props.resize(device.queues.queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device.physDeviceHandle, &device.queues.queueFamilyCount, device.queues.props.data());
+    for(size_t i = 0; i < device.queues.queueFamilyCount ; ++i) {
+        std::cout << "Queue Fam # " << i << ":\n";
+        std::cout << "GRAPHICS: " << ((device.queues.props[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) != 0) << "\n";
+        std::cout << "COMPUTE: " << ((device.queues.props[i].queueFlags & VK_QUEUE_COMPUTE_BIT)   != 0) << "\n";
+        std::cout << "TRANSFER: " << ((device.queues.props[i].queueFlags & VK_QUEUE_TRANSFER_BIT) != 0) << "\n";
+        std::cout << "# of queue: " << device.queues.props[i].queueCount << "\n\n";
+    }
+
+    // TODO: select highest number of COMPUTE queues as compute Queue
+    {
+        size_t i = 0;
+        for (const auto& queue : device.queues.props) {
+            if(((queue.queueFlags & VK_QUEUE_GRAPHICS_BIT)!=0) && device.queues.graphicsIndex == -1) {
+                device.queues.graphicsIndex = i;
+            }
+            if(((queue.queueFlags & VK_QUEUE_COMPUTE_BIT)!=0) && device.queues.graphicsIndex == -1) {
+                device.queues.computeIndex = i;
+            }
+        }
+        if(device.queues.computeIndex == device.queues.graphicsIndex || device.queues.graphicsIndex == -1 || device.queues.computeIndex == -1) {
+            std::cout << "Error assinging queue indices!\n";
+            exit(EXIT_FAILURE);
+        } else {
+            std::cout << "Assigned Queues:\ngraphics queue idx: " << device.queues.graphicsIndex << "\n";
+            std::cout << "compute queue idx: " << device.queues.computeIndex << "\n";
+        }
+    }
+    {
+        // graphics queue (for rt?)
+        device.queues.createInfo[0].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        device.queues.createInfo[0].pNext = NULL;
+        device.queues.createInfo[0].flags = 0;
+        device.queues.createInfo[0].queueFamilyIndex = device.queues.graphicsIndex;
+        device.queues.createInfo[0].queueCount = 1;
+        device.queues.createInfo[0].pQueuePriorities = &device.queues.priority;
+
+        // compute queue create acc structure!
+        device.queues.createInfo[0].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        device.queues.createInfo[0].pNext = NULL;
+        device.queues.createInfo[0].flags = 0;
+        device.queues.createInfo[0].queueFamilyIndex = device.queues.computeIndex;
+        device.queues.createInfo[0].queueCount = 1;
+        device.queues.createInfo[0].pQueuePriorities = &device.queues.priority;
+    }
+    {   
+        device.createInfo.bufferDevAdressFeatures.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_BUFFER_DEVICE_ADDRESS_FEATURES;
+        device.createInfo.bufferDevAdressFeatures.pNext = NULL;
+        device.createInfo.bufferDevAdressFeatures.bufferDeviceAddress = VK_TRUE;
+        device.createInfo.bufferDevAdressFeatures.bufferDeviceAddressCaptureReplay = VK_FALSE;
+        device.createInfo.bufferDevAdressFeatures.bufferDeviceAddressMultiDevice = VK_FALSE;
+
+        device.createInfo.rtPipelineFeatures.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_ACCELERATION_STRUCTURE_FEATURES_KHR;
+        device.createInfo.rtPipelineFeatures.pNext = &device.createInfo.bufferDevAdressFeatures;
+        device.createInfo.rtPipelineFeatures.rayTracingPipeline = VK_TRUE;
+        device.createInfo.rtPipelineFeatures.rayTracingPipelineShaderGroupHandleCaptureReplay = VK_FALSE;
+        device.createInfo.rtPipelineFeatures.rayTracingPipelineShaderGroupHandleCaptureReplayMixed = VK_FALSE;
+        device.createInfo.rtPipelineFeatures.rayTracingPipelineTraceRaysIndirect = VK_FALSE;
+        device.createInfo.rtPipelineFeatures.rayTraversalPrimitiveCulling = VK_FALSE; 
+
+
+        device.createInfo.accStructFeatures.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_ACCELERATION_STRUCTURE_FEATURES_KHR;
+        device.createInfo.accStructFeatures.pNext = &device.createInfo.rtPipelineFeatures;
+        device.createInfo.accStructFeatures.accelerationStructure = VK_TRUE;
+        device.createInfo.accStructFeatures.accelerationStructureCaptureReplay = VK_FALSE;
+        device.createInfo.accStructFeatures.accelerationStructureIndirectBuild = VK_FALSE;
+        device.createInfo.accStructFeatures.accelerationStructureHostCommands = VK_FALSE;
+        device.createInfo.accStructFeatures.descriptorBindingAccelerationStructureUpdateAfterBind = VK_FALSE; // TODO: Update here if the acc changes!
+    }
+
+
 }
+
--- src/util/host.hpp
+++ src/util/host.hpp
@@ -2,6 +2,7 @@
 
 #include "vulkan/vulkan.hpp"
 #include <cstdint>
+#include <vector>
 #include <vulkan/vulkan_core.h>
 
 class Application {
@@ -19,13 +20,40 @@ class Application {
     VkApplicationInfo applicationInfo = {};
     VkInstanceCreateInfo instanceCreateInfo = {};
     struct {
-        VkPhysicalDevice *device = VK_NULL_HANDLE;
+        VkPhysicalDevice physDeviceHandle = VK_NULL_HANDLE;
+        VkPhysicalDeviceFeatures physDeviceFeatures;
+        struct {
+            uint32_t queueFamilyCount = 0;
+            std::vector<VkQueueFamilyProperties> props;
+            const float priority = 1.0f;
+            const uint32_t nQueueInfos = 2; // TODO: Magic number :(
+            std::array<VkDeviceQueueCreateInfo,2> createInfo;
+            uint32_t graphicsIndex = -1;
+            uint32_t computeIndex = -1;
+        } queues;
+        VkPhysicalDeviceMemoryProperties memoryProperties;
+        const std::vector<const char*> enabledExtensions = {
+            "VK_KHR_ray_tracing_pipeline",
+            "VK_KHR_acceleration_structure",
+            "VK_KHR_spirv_1_4",
+            "VK_KHR_shader_float_controls", // TODO: required?
+            "VK_KHR_buffer_device_address",
+            "VK_KHR_deferred_host_operations",
+            "VK_KHR_pipeline_library"
+        };
+        struct {
+            VkPhysicalDeviceBufferDeviceAddressFeatures bufferDevAdressFeatures;
+            VkPhysicalDeviceRayTracingPipelineFeaturesKHR rtPipelineFeatures;
+            VkPhysicalDeviceAccelerationStructureFeaturesKHR accStructFeatures;
+            VkDeviceCreateInfo info;
+        } createInfo;
+    } device;
+    struct {
         uint32_t deviceCount = 0;
         std::vector<VkPhysicalDevice> listOfDevices;
+        std::vector<VkPhysicalDeviceProperties> deviceProperties;
+        VkPhysicalDeviceMemoryProperties memoryProperties;
+
     } phyDevices;
-    struct {
-        uint32_t graphicsQueueIndex = -1;
-        uint32_t computeQueueIndex = -1;
-        uint32_t queueFamilyCount = 0;
-    } queues;
+
 };
